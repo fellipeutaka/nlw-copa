4f310bd12da49d1ee1f1c2c01645c91dd28f713b
fellipeutaka <fellipeutaka@gmail.com>
2022-11-02 15:07:19 -0300

    updated fallback

--- packages/apps/web/src/hooks/useFetch.ts
+++ packages/apps/web/src/hooks/useFetch.ts
@@ -1,11 +1,19 @@
 import { api } from "@nlw-copa/axios-config";
 import useSWR from "swr";
+import type { BareFetcher, PublicConfiguration } from "swr/dist/types";
 
-export function useFetch<Data>(path: string) {
-  const { data, error, mutate } = useSWR<Data>(path, async (url) => {
-    const { data } = await api.get(url);
-    return data;
-  });
+export function useFetch<Data, Error = any>(
+  path: string,
+  config?: Partial<PublicConfiguration<Data, Error, BareFetcher<Data>>>
+) {
+  const { data, error, mutate } = useSWR<Data>(
+    path,
+    async (url) => {
+      const { data } = await api.get(url);
+      return data;
+    },
+    config
+  );
 
   const isLoading = !data && !error;
 

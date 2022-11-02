cbe8c54d0ab1faf902ff70896953248ebdc38655
fellipeutaka <fellipeutaka@gmail.com>
2022-11-02 14:29:27 -0300

    Changed axios config base URL

--- packages/shared/axios-config/index.ts
+++ packages/shared/axios-config/index.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://nlw-copa.herokuapp.com"
+    : "http://localhost:3333";
+
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL,
 });
 
 export { api, axios };
